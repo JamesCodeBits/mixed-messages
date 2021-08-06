09b95c4fa804d891a68d2998558f0606c2881a41
JamesCodebits <85015563+JamesCodeBits@users.noreply.github.com>
2021-08-06 22:57:01 +0100

    version 1.0

---
+++ README.md
@@ -0,0 +1,32 @@
+## Mixed Messages - Monster Edition
+
+* A suggestion generator brought to you by your favourite monsters! 
+
+
+## Built With 
+    javascript
+
+## Setup
+
+check node is installed
+
+    node -v
+
+clone repo and navigate to app directory
+
+
+    git clone https://github.com/JamesCodeBits/mixed-messages.git && cd mixed-messages/scripts
+
+run app
+
+    node app.js
+
+
+## Improvements
+* Publish on GitHub pages
+
+## Acknowledgements
+* [Codeacademy](https://www.codeacademy.com)
+
+## Licence
+* Use freely...just don't scare yourself! 
--- scripts/content.js
+++ scripts/content.js
@@ -1,8 +1,8 @@
 // generate content for random message function
 const content = {
     monster: ['Count Dracula', 'Dr Frankenstein', 'The Blob', 'Ghoul-Ra', 'Pumpkin Head'],
-    suggestions: ['highly recommends you', 'tentatively suggests you', 'enthusticially insists you', 'monsterously implores that you', 'demands menacingly with a creepy smile that you' ],
-    activities: ['eat cold worms on mouldy toast - comfort food, mmmmm!', 'cuddle a tarantula - two in fact!', 'drink a gallon of squashed pitslugs!', 'comb the living armpit hair of the deliciously foul Skunk-Wafter', 'take a stinkin bath in the fresh wet dung of crypt-bats']
+    suggestions: ['highly recommends you', 'tentatively suggests you', 'enthusticially insists you', 'monsterously implores that you', 'demands menacingly that you' ],
+    activities: ['eat cold worms on mouldy toast - comfort food, mmmmm!', 'mouth cuddle a tarantula - two in fact!', 'drink a gallon of juiced pit-slug guts - it will give you a nice unhealthy glow for days!', 'comb the living armpit hair of the diabolocial Skunk-Wafter - do not tickle it though!', 'have a necro-poop bath - you are starting to smell a bit fresh!']
 }
 
 export { content }
